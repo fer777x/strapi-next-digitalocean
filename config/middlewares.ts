59700ae9786c1589d717ef80336cd7641563d200
fernando <archivos.fer1@gmail.com>
2023-08-25 18:12:13 -0500

    strapi security fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,24 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "*.digitaloceanspaces.com"
+          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -10,3 +28,5 @@ export default [
   'strapi::favicon',
   'strapi::public',
 ];
+
+
